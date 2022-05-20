56114ff44f076f06b4c62749c3120093237444e9
gospelin <gospelin.gi@gmail.com>
2022-05-21 00:07:53 +0100

    update main.h

--- main.h
+++ main.h
@@ -52,10 +52,11 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
-char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
 char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
+
 
 #endif
