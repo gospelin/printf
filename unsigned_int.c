818535fcac470c8a3787122af86489156d7257da
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-21 11:35:27 +0000

    Adding unsigned_int file

---
+++ unsigned_int.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_b - takes an unsigned int and prints it in binary notation
+ * @b: unsigned in to print
+ *
+ * Return: number of digits printed
+ */
+int print_b(va_list b)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	n = va_arg(b, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
