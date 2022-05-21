989dec3a6507de05a81c6155dd361ceac583252b
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-21 12:52:18 +0000

    Updated

--- main.h
+++ main.h
@@ -21,5 +21,17 @@ int _printf(const char *format, ...);
 void buffer(char *s, char x, int *index);
 int print_c(va_list a, char *s, int *index);
 int print_s(va_list a, char *s, int *index);
+int print_por(va_list a, char *s, int *index);
+int print_id(va_list a, char *s, int *index);
+int print_bin(va_list a, char *s, int *index);
+int print_u(va_list a, char *s, int *index);
+int print_o(va_list a, char *s, int *index);
+int print_x(va_list a, char *s, int *index);
+int print_X(va_list a, char *s, int *index);
+int print_S(va_list a, char *s, int *index);
+int switch_H(int sum, int *h, int y);
+int print_R(va_list a, char *s, int *index);
+int print_r(va_list a, char *s, int *index);
+int print_p(va_list a, char *s, int *index);
 
 #endif /* _MAIN_H_ */
