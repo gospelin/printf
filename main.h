69905dad066a7d59690cf7421c046c42f6780e30
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-21 11:25:29 +0000

    Updated

--- main.h
+++ main.h
@@ -17,6 +17,7 @@ typedef struct print
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+static int (*check_specifiers(const char *format))(va_list);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
