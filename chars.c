00c4f898ee5e75aa53c8f3c60b520ae76c0d0840
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-21 12:34:58 +0000

    Updated

--- chars.c
+++ chars.c
@@ -52,3 +52,55 @@ int print_por(va_list a, char *s, int *index)
 	buffer(s, '%', index);
 	return (1);
 }
+/**
+ * print_por - print character %
+ * @a: element of va_list type
+ * @s: buffer
+ * @index: position on buffer
+ * Return: number of "characters" of element called
+ */
+int print_por(va_list a, char *s, int *index)
+{
+	(void)a;
+	buffer(s, '%', index);
+	return (1);
+}
+/**
+ * print_id - print numbers in base 10 (integers)
+ * @a: element of va_list type
+ * @s: buffer
+ * @index: position on buffer
+ * Return: number of "characters" of element called
+ */
+int print_id(va_list a, char *s, int *index)
+{
+	unsigned int j, cont = 1;
+	unsigned int var1, num, var2, var3 = 1;
+	int x, y = 0;
+
+	x = va_arg(a, int);
+	var2 = x;
+
+	if (x < 0)
+	{
+		buffer(s, '-', index);
+		var2 = x * (-1);
+		y++;
+	}
+	num = var2;
+	while (num > 9)
+	{
+		num = num / 10;
+		cont++;
+		var3 = var3 * 10;
+	}
+	for (j = 1; j <= cont; j++)
+	{
+		var1 = var2 / var3;
+		var2 = var2 % var3;
+		var3 = var3 / 10;
+		y++;
+		buffer(s, ('0' + var1), index);
+	}
+	return (y);
+}
