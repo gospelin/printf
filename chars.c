0740f5ba85c511e162cb0d60a039ac42844fc330
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-21 12:41:12 +0000

    Updated

--- chars.c
+++ chars.c
@@ -91,3 +91,53 @@ int print_id(va_list a, char *s, int *index)
 	}
 	return (y);
 }
+/**
+ * print_bin - print numbers in base 2 (binary)
+ * @a: element of va_list type
+ * @s: buffer
+ * @index: position on buffer
+ * Return: number of "characters" of element printed
+ */
+int print_bin(va_list a, char *s, int *index)
+{
+	int x, c, d, y = 0, aux = 0;
+	int arr[32] = {0};
+
+	aux = va_arg(a, int);
+	x = aux;/* The original decimal number*/
+	if (aux == 0)
+	{
+		buffer(s, '0', index);
+		return (1);
+	}
+	if (aux < 0)
+	{
+		x = -(x + 1);/*Add 1 to obtain two's complement*/
+	}
+	for (c = 0; x > 0; c++)/*Divide the number by 2 and store in the array*/
+	{
+		arr[c] = x % 2;
+		x = x / 2;
+	}
+	if (aux >= 0)
+	{
+		for (d = c - 1; d >= 0; d--)/*Prints since the last to the 1st pos*/
+		{
+			buffer(s, ('0' + arr[d]), index);
+			y++;
+		}
+	}
+	else
+	{
+		for (d = 31; d >= 0; d--)/*Finds the one's complement for the number*/
+		{
+			if (arr[d] == 1)
+				arr[d] = 0;
+			else if (arr[d] == 0)
+				arr[d] = 1;
+			buffer(s, ('0' + arr[d]), index);
+			y++;
+		}
+	}
+	return (y);
+}
