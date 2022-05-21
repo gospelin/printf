85b3592093fa7e010a81f3ad264e850f206612a6
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-21 10:15:59 +0000

    Updated

--- main.h
+++ main.h
@@ -1,11 +1,22 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+/**
+  * struct print - struct to print data types
+  * @t: data type
+  * @f: function
+  */
+
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_f;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-void check_specifier(const char *format, va_list args);
+int print_c(va_list c);
+int print_s(va_list s);
 
 #endif /* _MAIN_H_ */
